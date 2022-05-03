91ce15c1c021ae70933086525636640500700e53
tommy030314 <79099285+tommy030314@users.noreply.github.com>
2022-05-03 13:04:42 +0900

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,12 +49,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
-    },
-    {
-      name: 'Telegram',
-      link: 'https://t.me/realSpencerWoo',
+      name: 'Arcalive',
+      link: 'https://arca.live/b/yuzusoft',
     },
   ],
 
